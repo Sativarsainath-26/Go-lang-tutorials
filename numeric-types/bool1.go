33b2d4017fc5e227c5e495c7f88f72626a891d11
sainath <sativar.sainath@gmail.com>
2022-06-29 11:59:05 +0530

    updated boolean program

---
+++ numeric-types/bool1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 42
+	b := 42
+	fmt.Println(a == b) // output is true which is a boolean value. we can see here the expression is evaluated to a booelan value
+
+	// In the go lang there is no === triple equal.
+	// In the go lang single equal is an = assignment operator
+	// In the go lang double equal is an == conditional operator.
+
+}
